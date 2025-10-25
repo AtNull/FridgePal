69689265551bd7e1f4f52b5232523560f2945bdf
AtNull <brajkovicandjela1997@gmail.com>
2025-10-25 19:01:57 +0200

    basic api

--- api/api.js
+++ api/api.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const mockData = [
+  {
+    id:'1',
+    name: 'Lime',
+    purchaseDate: '2025-10-01T09:00:00Z',
+    expiryDate: '2025-11-14T09:00:00Z',
+    quantity: 2
+  },
+];
+
+app.get('/api/items', (_, res) => {
+  res.json(mockData);
+});
+
 app.listen(port, '0.0.0.0', () => {
   console.log(`Listening at http://0.0.0.0:${port}`);
 });
