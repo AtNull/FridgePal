e61eec97c715c4db8012f133cfe7508dfac6bed0
AtNull <brajkovicandjela1997@gmail.com>
2025-10-26 23:36:58 +0100

    hide status bar background

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fridge_pal/ui/screens/home.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light
+    )
+  );
+
   runApp(ProviderScope(child: const MainApp()));
 }
 
