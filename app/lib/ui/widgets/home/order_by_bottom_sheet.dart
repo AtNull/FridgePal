7fbf6fc2e3d96734d9473f2ec3150f5219ee8334
AtNull <brajkovicandjela1997@gmail.com>
2025-10-27 01:19:57 +0100

    display selected order option

--- app/lib/ui/widgets/home/order_by_bottom_sheet.dart
+++ app/lib/ui/widgets/home/order_by_bottom_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fridge_pal/providers/home_screen_providers.dart';
+import 'package:fridge_pal/util/theme_constants.dart';
 
 class OrderByBottomSheet extends StatelessWidget {
   final Order selectedOrder;
@@ -11,17 +12,24 @@ class OrderByBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        children: [
-          for (final order in Order.values)
-            TextButton(
-              onPressed: () {
-                onSelected(order);
-                Navigator.pop(context);
-              },
-              child: Text(order.label)
-            )
-        ],
+      child: Padding(padding: EdgeInsetsGeometry.symmetric(horizontal: spacing),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: cornerRadius),
+            for (final order in Order.values)
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: order == selectedOrder ? Theme.of(context).primaryColor : null
+                ),
+                onPressed: () {
+                  onSelected(order);
+                  Navigator.pop(context);
+                },
+                child: Text(order.label)
+              )
+          ],
+        )
       )
     );
   }
