9f8cde8a213989c946e420cb301c3e7eec4d0e71
AtNull <brajkovicandjela1997@gmail.com>
2025-10-27 01:18:34 +0100

    global themes

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fridge_pal/ui/screens/home.dart';
+import 'package:fridge_pal/util/theme_constants.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(
@@ -19,7 +20,75 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          surface: Color(0xFFF8FFF5),
+          primary: Color(0xFFCAEAB4),
+          onPrimary: Colors.black,
+          error: Color(0xFFB00020)
+        ),
+        fontFamily: 'Lexend',
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Color(0xFFCAEAB4),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(cornerRadius)
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(cornerRadius),
+            borderSide: const BorderSide(color: Colors.transparent),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(cornerRadius),
+            borderSide: const BorderSide(color: Colors.black, width: 1),
+          ),
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: cornerRadius,
+          ),
+        ),
+        iconButtonTheme: IconButtonThemeData(
+          style: IconButton.styleFrom(
+            fixedSize: Size(mediumWidgetHeight, mediumWidgetHeight),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(cornerRadius),
+            ),
+          )
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.black,
+            minimumSize: const Size(64, mediumWidgetHeight),
+            padding: const EdgeInsets.symmetric(horizontal: cornerRadius),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(cornerRadius),
+            ),
+          )
+        ),
+        filledButtonTheme: FilledButtonThemeData(
+          style: FilledButton.styleFrom(
+            foregroundColor: Colors.black,
+            minimumSize: const Size(64, mediumWidgetHeight),
+            padding: const EdgeInsets.symmetric(horizontal: cornerRadius),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(cornerRadius),
+            ),
+          ),
+        ),
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Colors.black,
+          selectionHandleColor: Colors.black,
+        ),
+        tabBarTheme: TabBarThemeData(
+          labelColor: Colors.black,
+          dividerColor: Colors.transparent,
+          indicator: BoxDecoration(
+            color: const Color(0xFFCAEAB4),
+            borderRadius: BorderRadius.circular(cornerRadius),
+          ),
+          indicatorSize: TabBarIndicatorSize.tab,
+        )
+      ),
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
